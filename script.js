32a123d749fc756969a61a8dcf45f277eaf11c9f
Thedraxx <thedraxxgames@gmail.com>
2021-12-28 12:39:24 -0300

    Se mod. el titulo a front-end dev. y la foto de perfil

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer"],
+        strings: ["Front-end Developer"],
         typeSpeed: 100,
         backSpeed: 80,
         loop: false
